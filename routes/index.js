c2d200a6355481f1668fb9fcc07661271cc6a05c
MuhamedShahabas <muhamedshahabasc@gmail.com>
2023-01-19 09:58:38 +0530

    Router codes formatted

--- routes/index.js
+++ routes/index.js
@@ -6,16 +6,18 @@ const landingPage = require("../controllers/index/landingPage");
 const product = require("./../controllers/index/product");
 const productListing = require("../controllers/index/productListing");
 
+// Landing Page
 router.get("/", landingPage.viewAll);
 
+// Our collection
 router
   .route("/products")
   .get(productListing.ourCollection)
   .patch(productListing.current)
   .put(productListing.search);
-
 router.get("/categories/:id", productListing.categories);
 
+// Single product Page
 router
   .route("/products/:id")
   .get(objectIdCheck, product.view)
